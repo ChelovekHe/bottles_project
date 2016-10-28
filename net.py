2c037e7a0a5b7e9b92392c1b838ff10e4b09cf3b
Ruslan Vorobyev <bodyrof1@gmail.com>
2016-10-28 17:24:00 +0300

    Pep8 confirmed

--- net.py
+++ net.py
@@ -8,7 +8,7 @@ from keras.optimizers import SGD
 import img_to_nparr
 
 
-def net (X_test, mode=1):
+def net(X_test, mode=1):
 
     img_width, img_height = 64, 64
 
@@ -18,9 +18,9 @@ def net (X_test, mode=1):
     nb_validation_samples = 40
     nb_epoch = 5
 
-
     model = Sequential()
-    model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(3, img_width, img_height)))
+    model.add(Convolution2D(32, 3, 3, border_mode='same',
+            input_shape=(3, img_width, img_height)))
     model.add(Activation('relu'))
     model.add(Convolution2D(32, 3, 3))
     model.add(Activation('relu'))
@@ -46,27 +46,27 @@ def net (X_test, mode=1):
                   optimizer=sgd,
                   metrics=['accuracy'])
 
-    train_datagen = ImageDataGenerator(rescale=1./255)
+    train_datagen = ImageDataGenerator(rescale=1. / 255)
 
-    test_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
 
     train_generator = train_datagen.flow_from_directory(
-            train_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=32)
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32)
 
     validation_generator = test_datagen.flow_from_directory(
-            validation_data_dir,
-            target_size=(img_width, img_height),
-            batch_size=32)
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32)
 
     if mode == 0:
         model.fit_generator(
-                train_generator,
-                samples_per_epoch=nb_train_samples,
-                nb_epoch=nb_epoch,
-                validation_data=validation_generator,
-                nb_val_samples=nb_validation_samples)
+            train_generator,
+            samples_per_epoch=nb_train_samples,
+            nb_epoch=nb_epoch,
+            validation_data=validation_generator,
+            nb_val_samples=nb_validation_samples)
 
         model.save_weights('weights1.h5')
     else:
@@ -74,15 +74,15 @@ def net (X_test, mode=1):
 
     list_of_list_of_class = []
     list_of_list_of_proba = []
-    
+
     for ind in range(len(X_test)):
-        list_of_class = model.predict_classes(X_test[ind], verbose = 0)
-        list_of_proba = model.predict_proba(X_test[ind], verbose = 0)
+        list_of_class = model.predict_classes(X_test[ind], verbose=0)
+        list_of_proba = model.predict_proba(X_test[ind], verbose=0)
         list_of_list_of_class.append(list_of_class)
         list_of_list_of_proba.append(list_of_proba)
-    
+
     return list_of_list_of_class, list_of_list_of_proba
 
 if __name__ == '__main__':
-	X_test = img_to_nparr.img_to_nparr('/home/ruslan/datas2/validation/5')
-	net(X_test)
+    X_test = img_to_nparr.img_to_nparr('/home/ruslan/datas2/validation/5')
+    net(X_test)
